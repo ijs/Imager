d4a526e9f121d6658fe7b4b186af6af3a389a886
Jerret <321jiangtao@gmail.com>
2016-11-28 17:41:44 +0800

    fix object.assgin polyfill

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import * as EXIF from 'exif-js'
+if (typeof Object.assign != 'function') {
+	Object.assign = function(target) {
+		'use strict'
+		if (target == null) {
+			throw new TypeError('Cannot convert undefined or null to object')
+		}
+		
+		target = Object(target)
+		for (var index = 1; index < arguments.length; index++) {
+			var source = arguments[index]
+			if (source != null) {
+				for (var key in source) {
+					if (Object.prototype.hasOwnProperty.call(source, key)) {
+						target[key] = source[key]
+					}
+				}
+			}
+		}
+		return target
+	}
+}
 export default class Imager {
 	constructor(opts) {
 		const defaults = {
